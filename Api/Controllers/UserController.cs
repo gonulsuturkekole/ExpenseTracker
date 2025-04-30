48dcb96e1818cc986ba93f82bdc8f2c30865a304
Gonul Su Turkekole <turkekolegonulsu@gmail.com>
2025-05-01 00:44:29 +0300

    user created

---
+++ Api/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿namespace ExpenseTracker.Api.Controllers;
+
+using ExpenseTracker.Base;
+using ExpenseTracker.Business.Cqrs;
+using ExpenseTracker.Schema;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/users")]
+public class UsersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UsersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public async Task<ApiResponse<UserResponse>> Post([FromBody] UserRequest model)
+    {
+        var result = await _mediator.Send(new CreateUserCommand(model));
+        return result;
+    }
+}
