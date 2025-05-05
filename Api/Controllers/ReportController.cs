f0c7aa0bf3f32425d6e38ea19ce76c215f48a32e
Gonul Su Turkekole <turkekolegonulsu@gmail.com>
2025-05-05 03:12:47 +0300

    report controller created

---
+++ Api/Controllers/ReportController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Api.Controllers;
+
+[ApiController]
+[Route("api/reports")]
+public class ReportController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ReportController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> ExpenseReport()
+    {
+        return Ok();
+    }
+}
